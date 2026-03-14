a9705d8f0422970359e144336a10ea82c2db8286
LeonardoBizzoni <leo2002714@gmail.com>
2026-03-14 10:34:19 +0100

    ROS2<->STM32 message passing

--- pioneer_workstation_ws/src/pioneer3DX_controller/src/p3dx_node.cpp
+++ pioneer_workstation_ws/src/pioneer3DX_controller/src/p3dx_node.cpp
@@ -8,17 +8,48 @@ P3DX_Controller_Node::P3DX_Controller_Node() :
   subscriber_command_velocity(this->create_subscription<geometry_msgs::msg::Twist>("p3dx_command_velocity", 10,
                                                                                    std::bind(&P3DX_Controller_Node::callback_subscribe_command_velocity,
                                                                                              this, std::placeholders::_1))),
-  publisher_odometry_timer(this->create_wall_timer(50ms, std::bind(&P3DX_Controller_Node::callback_publish_odometry, this)))
+  publisher_odometry_timer(this->create_wall_timer(50ms, std::bind(&P3DX_Controller_Node::callback_publish_odometry, this))),
+  stm32_running(false)
 {
-  this->declare_parameter("frame_id_odometry", std::string("p3dx_controller_odometry"));
   assert(this->serial_fd != -1);
+
+  this->declare_parameter("frame_id_odometry", std::string("p3dx_controller_odometry"));
+
+  // TODO(lb): force the switch to config mode maybe?
+  //           switching to run mode shouldn't happen now, this is just for testing
+  FMW_Message response, msg_run;
+  msg_run.header.type = FMW_MessageType_ModeChange_Run;
+  msg_run.header.crc = (uint32_t)-1;
+  (void)::write(this->serial_fd, &msg_run, sizeof(FMW_Message));
+  for (uint32_t bytes_read = 0; bytes_read < sizeof(response);) {
+    ssize_t n = ::read(this->serial_fd, ((uint8_t*)&response) + bytes_read, sizeof(response) - bytes_read);
+    if (n >= 0) { bytes_read += (uint32_t)n; }
+  }
+  stm32_message_print(&response);
+  assert(response.header.type == FMW_MessageType_Response);
+  assert(response.response.result == FMW_Result_Ok || response.response.result == FMW_Result_Error_Command_NotAvailable);
+  this->stm32_running = true;
 }
 
 void P3DX_Controller_Node::callback_publish_odometry(void)
 {
+  if (!this->stm32_running) { return; }
+
+  FMW_Message response, msg_get_status;
+  msg_get_status.header.type = FMW_MessageType_Run_GetStatus;
+  msg_get_status.header.crc = (uint32_t)-1;
+  (void)::write(this->serial_fd, &msg_get_status, sizeof(FMW_Message));
+  for (uint32_t bytes_read = 0; bytes_read < sizeof(response);) {
+    ssize_t n = ::read(this->serial_fd, ((uint8_t*)&response) + bytes_read, sizeof(response) - bytes_read);
+    if (n >= 0) { bytes_read += (uint32_t)n; }
+  }
+  assert(response.header.type == FMW_MessageType_Response);
+  stm32_message_print(&response);
+
+
   nav_msgs::msg::Odometry output;
   output.header.frame_id = this->get_parameter("frame_id_odometry").as_string();
-  // output.header.stamp = ;            // TODO(lb): generate some kind of current timestamp
+  output.header.stamp = rclcpp::Time(0, 0, this->get_clock()->now().get_clock_type());
 
   // output.pose.pose.position.x = ;    // TODO(lb): fill with pioneer data
   // output.pose.pose.position.y = ;    // TODO(lb): fill with pioneer data
@@ -37,10 +68,51 @@ void P3DX_Controller_Node::callback_publish_odometry(void)
   assert(output.twist.twist.angular.z == 0);
 
   this->publisher_odometry->publish(output);
-  RCLCPP_INFO(this->get_logger(), "Hello, World!");
 }
 
 void P3DX_Controller_Node::callback_subscribe_command_velocity(const geometry_msgs::msg::Twist::SharedPtr cmd)
 {
   (void)cmd;
+}
+
+void P3DX_Controller_Node::stm32_message_print(const FMW_Message *msg)
+{
+  static const char *message_types[] = {
+    #define X(Variant, Id) #Variant,
+    FMW_MESSAGE_TYPE_VARIANTS()
+    #undef X
+  };
+
+  static const char *result_types[] = {
+    #define X(Variant, Id) #Variant,
+    FMW_RESULT_VARIANTS()
+    #undef X
+  };
+
+  char buffer[512] = {0};
+  int32_t buffer_len = snprintf(buffer, sizeof(buffer),
+                                "FMW_Message {"
+                                "\n  header.type = %s"
+                                "\n  header.crc  = %u",
+                                message_types[msg->header.type], msg->header.crc);
+  assert(buffer_len > 0);
+  assert(buffer_len < sizeof(buffer));
+
+  switch (msg->header.type) {
+  case FMW_MessageType_Response: {
+    buffer_len += snprintf(buffer + buffer_len, sizeof(buffer) - buffer_len,
+                           "\n  response:"
+                           "\n    result       = %s"
+                           "\n    delta_millis = %d"
+                           "\n    ticks_left   = %d"
+                           "\n    ticks_right  = %d",
+                           result_types[msg->response.result], msg->response.delta_millis,
+                           msg->response.ticks_left, msg->response.ticks_right);
+    assert(buffer_len < sizeof(buffer));
+  } break;
+  default: {
+    assert(false && "unreachable");
+  } break;
+  }
+  RCLCPP_INFO(this->get_logger(), "%s\n}", buffer);
 }
--- pioneer_workstation_ws/src/pioneer3DX_controller/src/p3dx_node.hpp
+++ pioneer_workstation_ws/src/pioneer3DX_controller/src/p3dx_node.hpp
@@ -4,6 +4,10 @@
 #include <rclcpp/rclcpp.hpp>
 #include <nav_msgs/msg/odometry.hpp>
 
+extern "C" {
+#include "stm32_messages.h"
+}
+
 #include "main.hpp"
 
 struct P3DX_Controller_Node: public rclcpp::Node {
@@ -11,11 +15,15 @@ struct P3DX_Controller_Node: public rclcpp::Node {
   rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr publisher_odometry;
   rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr subscriber_command_velocity;
   rclcpp::TimerBase::SharedPtr publisher_odometry_timer;
+  bool stm32_running;
 
   P3DX_Controller_Node(void);
 
   void callback_publish_odometry(void);
   void callback_subscribe_command_velocity(const geometry_msgs::msg::Twist::SharedPtr cmd);
+
+  void stm32_message_print(const FMW_Message *msg);
 };
+
 
 #endif
